23d176f65e6ff80614014b25a243b3e59f883eb0
Murph <pandeiro@gmail.com>
2013-02-08 17:55:59 -0200

    bump version with write-tag! bug fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject claudio "0.1.0"
+(defproject claudio "0.1.1"
   :description "A simple utility for reading and writing textual ID3 tag data"
   :url "https://github.com/pandeiro/claudio"
   :dependencies [[org.clojure/clojure "1.5.0-RC2"]
