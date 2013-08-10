b94992db9c2b1d9567e3bb17bf700c9d2a39ba29
Murph <pandeiro@gmail.com>
2013-08-10 12:55:15 -0300

    bump clj to 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject claudio "0.1.1"
   :description "A simple utility for reading and writing textual ID3 tag data"
   :url "https://github.com/pandeiro/claudio"
-  :dependencies [[org.clojure/clojure "1.5.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org/jaudiotagger "2.0.3"]])
