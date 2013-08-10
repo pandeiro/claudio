9048148a53f179db6f2d6871654c02acd17f53a5
Murph <pandeiro@gmail.com>
2013-08-10 13:12:50 -0300

    added license

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject claudio "0.1.2"
   :description "A simple utility for reading and writing textual ID3 tag data"
   :url "https://github.com/pandeiro/claudio"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org/jaudiotagger "2.0.3"]])
