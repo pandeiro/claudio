ffc7f49e4010c9b52840ae372f9d529ab11ff10f
Murphy McMahon <pandeiro@gmail.com>
2014-09-11 17:56:59 -0300

    bump clj ver

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/pandeiro/claudio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org/jaudiotagger "2.0.3"]])
