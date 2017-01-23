cf635287f521e5dff41a08eee6ef9758ce807b09
Florin <florin@libertv.ro>
2017-01-23 11:14:07 +0100

    Create id3 tag if it doesn't exist
    `write-tag!` will silently fail if the file doesn't have an id3 tag (getTag() returns nil).
    Here, we create the tag if it doesn't exist and so it fixes the issue.

--- src/claudio/id3.clj
+++ src/claudio/id3.clj
@@ -45,7 +45,7 @@ Returns the updated tag map."
     (when-not (even? (count kvs))
       (throw (Exception. "Tag-value pairs must an even number")))
     (let [audio-file (org.jaudiotagger.audio.AudioFileIO/read f)]
-      (when-let [tag (.getTag audio-file)]
+      (when-let [tag (.getTagOrCreateAndSetDefault audio-file)]
         (doseq [[k v] (partition 2 kvs)]
           (let [field-key (->constant (or (and (keyword? k) (constantify k)) k))]
             (if v
